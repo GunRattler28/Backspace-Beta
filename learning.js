c96abb4dc40c9c47e023f28ed59534cc392e65ca
GunRattler28 <aayushdeepchand@gmail.com>
2026-01-26 17:41:33 +0000

    Multiple search results

--- learning.js
+++ learning.js
@@ -1,28 +1,25 @@
 const form = document.getElementById('form')
-form.addEventListener('submit', getMovie)
+const results = document.getElementById('results')
+form.addEventListener('submit', searchMovies)
 
-async function getId(movie) {
-    try {
-        const response = await fetch("https://api.themoviedb.org/3/search/movie?api_key=083d1d154463f65cbdd3f504c0b893d5&query=" + encodeURIComponent(movie))
-        if (response.status != 200) {
-            throw new Error(`${response.status} error`)
-        }
-        const data = await response.json()
-        return(data.results[0].id)        
-    } catch {
-        console.error("getId failed")
-        return null
-    }
-}
-
-async function getMovie(e) {
+async function searchMovies(e){
     e.preventDefault()
-    let title = document.getElementById('title').value
-    let id = await getId(title)
-    if (id == null){
-        console.error("ID is invalid");
+    const input = document.getElementById('title').value
+    const response = await fetch("https://api.imdbapi.dev/search/titles?query=" + encodeURIComponent(input))
+    const data = await response.json()
+    const allTitles = data.titles
+    results.innerHTML = ''
+    for (let i = 0; i < allTitles.length; i++) {
+        const movie = allTitles[i]
+        const line = document.createElement('div')
+        const poster = `<img src=${movie.primaryImage.url} class="poster">`
+        const title = movie.primaryTitle
+        line.innerHTML = poster + " " + title
+        line.onclick = function() {
+            const iframe = document.getElementById('iframe')
+            iframe.src = 'about:blank'
+            iframe.src = `https://111movies.com/movie/${movie.id}`
+        }
+        results.appendChild(line)
     }
-    console.log(id)
-    const iframe = document.getElementById('iframe')
-    iframe.src = `https://111movies.com/movie/${id}`
 }
